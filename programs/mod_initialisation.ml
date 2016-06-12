530c47f1deee6d3605cf2e84efffc4c84205e4d7
John Whitington <john@coherentgraphics.co.uk>
2016-06-12 14:43:00 +0100

    more

---
+++ programs/mod_initialisation.ml
@@ -0,0 +1,17 @@
+(* 1 Immutable value *)
+(*let x = 1 + 2 + 3
+
+(* x is free in f *)
+let f y = x + y
+
+let _ = f 0*)
+
+(*let _ = f 1*)
+
+(* 2. Mutable value *)
+let x = ref (5 + 4)
+
+let f y =
+  x := !x + y; !x
+
+let _ = f 0
