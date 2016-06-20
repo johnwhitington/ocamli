b0aff9c70d9f432f3fd12d1d770954991de367ae
John Whitington <john@coherentgraphics.co.uk>
2016-06-20 18:02:48 +0100

    more

--- test/test.ml
+++ test/test.ml
@@ -1,1 +1,20 @@
 (* tester *)
+let dir_listing directory =
+  Array.to_list (Sys.readdir directory)
+
+let command_and_print s = Printf.printf "%s%!" s; Sys.command s
+
+let exec = if Sys.os_type = "Unix" then "cpdf" else "cpdf.exe"
+
+let runtest filename =
+  prerr_string filename;
+  prerr_newline ()
+
+let runtests dir =
+  List.iter runtest (List.map (Filename.concat dir) (dir_listing dir))
+
+let _ =
+  match Sys.argv with
+  | [|_|] -> runtests "../programs"
+  | _ ->
+      prerr_string "Syntax:"
