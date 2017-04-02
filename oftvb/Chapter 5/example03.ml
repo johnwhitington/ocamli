b42ee5dbef8810a1273f512d20ad6aa6aeabf0ca
John Whitington <john@coherentgraphics.co.uk>
2017-04-02 18:17:44 +0100

    more

--- oftvb/Chapter 5/example03.ml
+++ oftvb/Chapter 5/example03.ml
@@ -13,6 +13,15 @@ let rec length x =
     [] -> 0
   | _::t -> 1 + length t
 
+let rec merge x y =
+  match x, y with
+    [], l -> l
+  | l, [] -> l
+  | hx::tx, hy::ty ->
+      if hx < hy
+        then hx :: merge tx (hy :: ty)
+        else hy :: merge (hx :: tx) ty
+
 let rec msort l =
   match l with
     [] -> []
