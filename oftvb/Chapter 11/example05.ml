1712d33c9291e7346218e093a9caa53de6ddd6e0
John Whitington <john@coherentgraphics.co.uk>
2017-04-04 20:04:58 +0100

    more

--- oftvb/Chapter 11/example05.ml
+++ oftvb/Chapter 11/example05.ml
@@ -1,52 +1,3 @@
-(*1*)
-type 'a tree =
-    Br of 'a * 'a tree * 'a tree
-  | Lf
-
-let rec size tr =
-  match tr with
-    Br (_, l, r) -> 1 + size l + size r
-  | Lf -> 0
-
-let rec total tr =
-  match tr with
-    Br (x, l, r) -> x + total l + total r
-  | Lf -> 0
-
-(*2*)
-type 'a tree =
-    Br of 'a * 'a tree * 'a tree
-  | Lf
-
-let max x y =
-  if x > y then x else y
-
-let rec maxdepth tr =
-  match tr with
-    Br (_, l, r) -> 1 + max (maxdepth l) (maxdepth r)
-  | Lf -> 0
-
-(*3*)
-type 'a tree =
-    Br of 'a * 'a tree * 'a tree
-  | Lf
-
-let rec list_of_tree tr =
-  match tr with
-    Br (x, l, r) -> list_of_tree l @ [x] @ list_of_tree r
-  | Lf -> []
-
-(*4*)
-type 'a tree =
-    Br of 'a * 'a tree * 'a tree
-  | Lf
-
-let rec tree_map f tr =
-  match tr with
-    Br (x, l, r) -> Br (f x, tree_map f l, tree_map f r)
-  | Lf -> Lf
-
-(*5*)
 type 'a tree =
     Br of 'a * 'a tree * 'a tree
   | Lf
@@ -58,25 +9,4 @@ let rec lookup tr k =
       if k = k' then v
       else if k < k' then lookup l k
       else lookup r k
-
-(*6*)
-type 'a tree =
-    Br of 'a * 'a tree * 'a tree
-  | Lf
-
-let rec lookup tr k =
-  match tr with
-    Lf -> None
-  | Br ((k', v), l, r) ->
-      if k = k' then Some v
-      else if k < k' then lookup l k
-      else lookup r k
-
-let rec insert tr k v =
-  match tr with
-    Lf -> Br ((k, v), Lf, Lf)
-  | Br ((k', v'), l, r) ->
-      if k = k' then Br ((k, v), l, r)
-      else if k < k' then Br ((k', v'), insert l k v, r)
-      else Br ((k', v'), l, insert r k v)
 
