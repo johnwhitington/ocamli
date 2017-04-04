0e0ff75f48007d12625f68db1cdec274639097a8
John Whitington <john@coherentgraphics.co.uk>
2017-04-04 19:19:54 +0100

    more

--- oftvb/Chapter 10/exercise04.ml
+++ oftvb/Chapter 10/exercise04.ml
@@ -1,22 +1,13 @@
-(*1*)
 type rect =
     Square of int
   | Rectangle of int * int
 
-(*2*)
-let area r =
-  match r with
-    Square s -> s * s
-  | Rectangle (w, h) -> w * h
-
-(*3*)
 let rotate r =
   match r with
     Rectangle (w, h) ->
       if w > h then Rectangle (h, w) else r
   | Square _ -> r
 
-(*4*)
 let width_of_rect r =
   match r with
     Square s -> s
@@ -45,72 +36,3 @@ let rec sort f l =
 
 let pack rs =
   sort rect_compare (map rotate rs)
-
-(*5*)
-type 'a sequence = Nil | Cons of 'a * 'a sequence
-
-let rec take n l =
-  if n = 0 then Nil else
-    match l with
-      Nil -> raise (Invalid_argument "take")
-    | Cons (h, t) -> Cons (h, take (n - 1) t)
-
-let rec drop n l =
-  if n = 0 then l else
-    match l with
-      Nil -> raise (Invalid_argument "drop")
-    | Cons (_, l) -> drop (n - 1) l
-
-let rec map f l =
-  match l with
-    Nil -> Nil
-  | Cons (h, t) -> Cons (f h, map f t)
-
-(*6*)
-let rec power x n =
-  if n = 0 then 1 else
-    if n = 1 then x else
-      x * power x (n - 1)
-
-type expr =
-    Num of int
-  | Add of expr * expr
-  | Subtract of expr * expr
-  | Multiply of expr * expr
-  | Divide of expr * expr
-  | Power of expr * expr
-
-let rec evaluate e =
-  match e with
-    Num x -> x
-  | Add (e, e') -> evaluate e + evaluate e'
-  | Subtract (e, e') -> evaluate e - evaluate e'
-  | Multiply (e, e') -> evaluate e * evaluate e'
-  | Divide (e, e') -> evaluate e / evaluate e'
-  | Power (e, e') -> power (evaluate e) (evaluate e')
-
-(*7*)
-let rec power x n =
-  if n = 0 then 1 else
-    if n = 1 then x else
-      x * power x (n - 1)
-
-type expr =
-    Num of int
-  | Add of expr * expr
-  | Subtract of expr * expr
-  | Multiply of expr * expr
-  | Divide of expr * expr
-  | Power of expr * expr
-
-let rec evaluate e =
-  match e with
-    Num x -> x
-  | Add (e, e') -> evaluate e + evaluate e'
-  | Subtract (e, e') -> evaluate e - evaluate e'
-  | Multiply (e, e') -> evaluate e * evaluate e'
-  | Divide (e, e') -> evaluate e / evaluate e'
-  | Power (e, e') -> power (evaluate e) (evaluate e')
-
-let evaluate_opt e =
-  try Some (evaluate e) with Division_by_zero -> None
