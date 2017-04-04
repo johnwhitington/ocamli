d755b95c376da607f8158d3ca8ba06416086dea9
John Whitington <john@coherentgraphics.co.uk>
2017-04-04 16:45:11 +0100

    more

--- oftvb/Chapter 7/exercise02.ml
+++ oftvb/Chapter 7/exercise02.ml
@@ -1,2 +1,14 @@
+let rec smallest_inner current found l =
+  match l with
+    [] ->
+      if found then current else raise Not_found 
+  | h::t ->
+      if h > 0 && h < current
+        then smallest_inner h true t
+        else smallest_inner current found t
+
+let smallest l =
+  smallest_inner max_int false l
+
 let smallest_or_zero l =
   try smallest l with Not_found -> 0
--- oftvb/Chapter 7/exercise05.ml
+++ oftvb/Chapter 7/exercise05.ml
@@ -1,4 +1,10 @@
 exception Complex
 
+let rec sqrt_inner x n =
+  if x * x > n then x - 1 else sqrt_inner (x + 1) n
+
+let sqrt n =
+  if n < 0 then raise Complex else sqrt_inner 1 n
+
 let safe_sqrt n =
   try sqrt n with Complex -> 0
