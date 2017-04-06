f1d4dd2ef5c29f1b1e396b7729a6a96ae4646418
John Whitington <john@coherentgraphics.co.uk>
2017-04-06 17:45:20 +0100

    more

--- oftvb/Chapter 6/exercise03.ml
+++ oftvb/Chapter 6/exercise03.ml
@@ -3,13 +3,6 @@ let rec map f l =
     [] -> []
   | h::t -> f h :: map f t
 
-let clip x =
-  if x < 1 then 1 else
-    if x > 10 then 10 else x
-
-let cliplist l =
-  map clip l
-
 let cliplist l =
   map
     (fun x ->
