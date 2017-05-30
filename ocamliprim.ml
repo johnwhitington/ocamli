d78caf85e089c0fa5ab3952aab78931d09f4aecd
John Whitington <john@coherentgraphics.co.uk>
2017-05-30 17:56:15 +0100

    Exceptions on autos now work

--- ocamliprim.ml
+++ ocamliprim.ml
@@ -18,6 +18,12 @@ let percent_word_size =
     ("%word_size", Fun (NoLabel, PatVar "*x", CallBuiltIn (None, "%word_size", [Var "*x"], f), []))
 *)
 
+(* Build a Raise () from a native OCaml exn. *)
+let of_real_ocaml = ref (fun (_ : Tinyocaml.env) _ -> Tinyocaml.Int 0)
+
+let exception_from_ocaml e =
+  !of_real_ocaml [] (ast ("raise (" ^ Printexc.to_string e ^ ")"))
+
 [%%auto {|external word_size : unit -> int = "%word_size"|}]
 [%%auto {|external int_size : unit -> int = "%int_size"|}]
 [%%auto {|external max_wosize : unit -> int = "%max_wosize"|}]
@@ -28,7 +34,7 @@ let percent_word_size =
 [%%auto {|external percent_boolnot : bool -> bool = "%boolnot"|}]
 [%%auto {|external percent_negint : int -> int = "%negint"|}]
 [%%auto {|external caml_ml_input_scan_line : in_channel -> int = "caml_ml_input_scan_line"|}]
-(*[%%auto {|external caml_create_string : int -> string = "caml_create_string"|}]*)
+[%%auto {|external caml_create_string : int -> string = "caml_create_string"|}]
 [%%auto {|external caml_create_bytes : int -> bytes = "caml_create_bytes"|}]
 [%%auto {|external caml_int64_float_of_bits : int64 -> float = "caml_int64_float_of_bits"|}]
 [%%auto {|external ignore : 'a -> unit = "%ignore"|}]
@@ -115,12 +121,6 @@ let caml_register_named_value =
 
 external array_safe_get : 'a array -> int -> 'a = "%array_safe_get"
 
-(* Build a Raise () from a native OCaml exn. *)
-let of_real_ocaml = ref (fun (_ : Tinyocaml.env) _ -> Tinyocaml.Int 0)
-
-let exception_from_ocaml e =
-  !of_real_ocaml [] (ast ("raise (" ^ Printexc.to_string e ^ ")"))
-
 let percent_array_safe_get =
   mk2 "%array_safe_get"
     (function [Array x; Int i] ->
@@ -129,17 +129,6 @@ let percent_array_safe_get =
        with
          e -> exception_from_ocaml e)
      | _ -> failwith "percent_array_safe_get")
-
-(* Our exceptions example - delete *)
-external caml_create_string : int -> string = "caml_create_string"
-
-let caml_create_string =
-  mk "caml_create_string"
-    (function [Int i] ->
-       begin try String (caml_create_string i) with
-         e -> exception_from_ocaml e
-       end
-     | _ -> failwith "caml_create_string")
 
 (* bytes *)
 external bytes_to_string : bytes -> string = "%bytes_to_string"
