8cd482e6b5891cc8342f30564678b07278643862
John Whitington <john@coherentgraphics.co.uk>
2017-07-12 15:46:49 +0100

    ppx_eval fixes

--- ppx_eval/ppx_eval.ml
+++ ppx_eval/ppx_eval.ml
@@ -4,6 +4,10 @@ open Asttypes
 open Parsetree
 open Longident
 
+let _ =
+ Ocamliprim.emulated := true;
+ Ocamlilib.load_library ()
+
 let extract_expression = function
   [{pstr_desc = Pstr_eval (e, _)}] -> e
 | _ -> failwith "ppx_eval: extract_expression"
