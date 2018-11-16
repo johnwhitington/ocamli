1da2ecc40f3c710122b811278804e0631172a96f
John Whitington <john@coherentgraphics.co.uk>
2018-11-16 17:40:34 +0000

    more

--- tppx/foo.ml
+++ tppx/foo.ml
@@ -1,4 +1,32 @@
-let () =
-  ignore ([Some Format.std_formatter] [@print_type]);
-  print_string "\n"
+let[@showtype] f (x [@showtype]) y = x + y [@showtype "result"]
+
+(*let[@showtype] rec merge (x [@showtype]) y =
+  match x, y [@showtype] with
+    [], l -> l
+  | l, [] -> l
+  | hx::tx, hy::ty ->
+      if hx < hy
+        then hx :: merge tx (hy :: ty)
+        else hy :: merge (hx :: tx) ty
+
+let rec take n l =
+  if n = 0 [@showtype] then [] else
+    match l with
+      h::t -> h :: take (n - 1) t
+    | [] -> failwith "take"
+
+let rec drop n l =
+  if n = 0 then l else
+    match l with
+      h::t -> drop (n - 1) t
+    | [] -> failwith "drop"
+
+let rec msort l =
+  match l with
+    [] -> []
+  | [x] -> [x]
+  | _ ->
+      let left = take (List.length l / 2) l
+      and right = drop (List.length l / 2) l in
+        merge (msort left) (msort right)*)
 
--- tppx/tppx_interpret.ml
+++ tppx/tppx_interpret.ml
@@ -12,23 +12,46 @@ let typedtree_to_file fn tstr =
 open Tast_mapper
 open Asttypes
 
-(* Example: replace any number 1 annotate with [@interpret] by the number 42, and remove the annotation *)
 let process tstr =
   let newmapper =
     {default with
-       expr = fun mapper expr ->
+       expr = (fun mapper expr ->
          match expr with
-         | {exp_attributes = [({txt = "print_type"}, _)];
+         | {exp_attributes = [({txt = "showtype"}, payload)];
             exp_type} as other ->
+              begin
+                match payload with
+                | PStr [{pstr_desc = Pstr_eval ({pexp_desc = Pexp_constant (Pconst_string (s, _)) }, _)}] ->
+                    Format.print_string (s ^ " : ")
+                | _ -> ()
+              end;
               Printtyp.type_expr Format.std_formatter exp_type;
               Format.print_newline ();
               default.expr mapper other
-         | other -> default.expr mapper other}
+         | other -> default.expr mapper other);
+      structure_item = (fun mapper stritem ->
+        match stritem with
+        | {str_desc = Tstr_value (_, [{vb_pat = {pat_desc = Tpat_var (ident, _)}; vb_expr}])} ->
+            Format.print_string (Ident.name ident ^ " : ");
+            Printtyp.type_expr Format.std_formatter vb_expr.exp_type;
+            Format.print_newline ();
+            default.structure_item mapper stritem
+        | other -> default.structure_item mapper other);
+      pat = (fun mapper pat ->
+        match pat with
+        | {pat_attributes = [({txt = "showtype"}, payload)];
+            pat_type;
+            pat_desc = Tpat_var (ident, _)} as other ->
+              Format.print_string (Ident.name ident ^ " : ");
+              Printtyp.type_expr Format.std_formatter pat_type;
+              Format.print_newline ();
+              default.pat mapper other
+         | other -> default.pat mapper other)
+    }
   in
     newmapper.structure newmapper tstr
 
 let _ =
-  print_endline "Tppx running...";
   match Sys.argv with
     [|_; infile; outfile|] ->
       typedtree_to_file outfile (process (typedtree_of_file infile))
