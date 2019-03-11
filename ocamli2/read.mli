b18ad01b51638f95b81ab5deb3595786ec73f7dd
John Whitington <john@coherentgraphics.co.uk>
2019-03-11 17:05:39 +0000

    more

--- ocamli2/read.mli
+++ ocamli2/read.mli
@@ -2,6 +2,8 @@ val to_ocaml_heap_value : Type.t' -> Obj.t
 
 val finaltype_of_typedtree : Typedtree.structure -> Type.t
 
+val finaltype_of_expression : Type.env -> Typedtree.expression -> Type.t
+
 val typedtree_of_string : ?filename:string -> string -> Typedtree.structure
 
 val read : string -> Type.t 
--- ocamli2/tppx_interpret/tppx_interpret.ml
+++ ocamli2/tppx_interpret/tppx_interpret.ml
@@ -1,17 +1,45 @@
 open Tast_mapper
 open Asttypes
 
+(* We pull the eval_full function out when we find it, at the top of the file,
+ * so that we have access to it to make up the fragment of typed tree which
+ * applies it to the program. *)
+let eval_full_function : Typedtree.expression option ref = ref None
+
 let newmapper argv =
   {default with
      expr = (fun mapper expr ->
        match expr with
        | {exp_attributes = [{attr_name = {txt = "interpret"}; attr_payload =  payload}];
-          exp_type} as other ->
+          exp_type;
+          exp_desc} ->
             print_endline "Found a [@interpret] annotation";
-            default.expr mapper other
+            (* We build a new expression to replace this one, with the same
+             * type, and remove the attribute. *)
+            (* 1. Make tinyocaml expression as a value. *)
+            let tinyocaml_expression =
+              Read.finaltype_of_expression [] expr 
+            in
+            (* 2. Make the typed-tree representation of the expression which
+             * calls eval_full on the tinyocaml representation, returning the
+             * value which is the result of the evaluation. i.e "eval_full
+             * <expr" *)
+            let exp_desc =
+              exp_desc
+            in
+            (* 3. This new little typed-tree fragment is spliced in to the node
+             * which was annotated, hopefully keeping all the invariants needed
+             * to allow the OCaml compiler to complete its job correctly and
+             * produce an executable. *)
+            {expr with exp_desc = exp_desc}
        | other -> 
-           print_endline "Not an [@interpret] annotation";
-           default.expr mapper other)}
+           default.expr mapper other);
+     structure_item = (fun mapper sitem ->
+        (* We are trying to find "let eval_full = Eval.eval_full". When we find
+         * it, we need to extract the function, making sure it has the correct
+         * type with it. *)
+        default.structure_item mapper sitem)
+  }
 
 let () =
   Tast_mapper.register "tppx_interpret" newmapper
