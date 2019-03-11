b4d260be2d1a35642b6e2ff58276d039dc28422a
John Whitington <john@coherentgraphics.co.uk>
2019-03-11 16:04:56 +0000

    more

--- ocamli2/example/c.ml
+++ ocamli2/example/c.ml
@@ -1,3 +1,9 @@
-let x = 1 + 2 * 3 [@interpret] in
-  Printf.printf "Result is %i\n" x
+(* This will be added by the PPX, eventually. We need it here so the TPPX can call it correctly to implement a [@interpret] annotation. *)
+let eval_full = Eval.eval_full 
+
+let x = 1 + 2 * 3 [@interpret]
+
+let _ =
+  Printf.printf "Result is %i\n" x;
+  exit 0
 
