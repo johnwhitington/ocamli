f411955409e3a82cba020fb0277ee7d6255de7f9
John Whitington <john@coherentgraphics.co.uk>
2019-03-19 18:50:53 +0000

    more

--- ocamli2/example/example.ml
+++ ocamli2/example/example.ml
@@ -1,8 +1,8 @@
 let global = function x -> x * 2
 
-let x =
+let x () =
   let y = 1 - 0 in
     (Random.int 50 + y + 2 * 3 + global 42 [@interpret]) + 4
 
 let _ =
-  Printf.printf "Result is %i\n" x
+  Printf.printf "Result is %i\n" (x ())
