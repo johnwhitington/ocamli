d22eeeb3b1486bb8de85d83b91cb195713b1c115
John Whitington <john@coherentgraphics.co.uk>
2019-03-20 16:19:39 +0000

    more

--- ocamli2/read.mli
+++ ocamli2/read.mli
@@ -6,4 +6,8 @@ val finaltype_of_expression : Type.env -> Typedtree.expression -> Type.t
 
 val typedtree_of_string : ?filename:string -> string -> Typedtree.structure
 
+val remove_links : Types.type_expr -> Types.type_expr
+
+val debug_type : Types.type_expr -> Types.type_expr
+
 val read : string -> Type.t 
